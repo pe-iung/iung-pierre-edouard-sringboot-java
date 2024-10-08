8843e83653a403f6ab628c90bb030f8213d7c128
pierre-edouard iung <contact@pierre-edouard.tech>
2024-10-08 15:51:20 +0200

    feat: create dataloader with object mapper

--- src/main/java/com/openclassrooms/P5/DataLoader.java
+++ src/main/java/com/openclassrooms/P5/DataLoader.java
@@ -1,4 +1,26 @@
 package com.openclassrooms.P5;
 
-public class DataLoader {
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final ObjectMapper objectMapper;
+
+    public DataLoader(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    //private final PersonRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        JsonNode jsonDataFile;
+
+
+    }
 }
