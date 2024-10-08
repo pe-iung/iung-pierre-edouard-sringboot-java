712dbafe68aae4b8e19333b8ee3e2510172e54b1
pierre-edouard iung <contact@pierre-edouard.tech>
2024-10-08 15:56:56 +0200

    feat: read json data from object mapper

--- src/main/java/com/openclassrooms/P5/DataLoader.java
+++ src/main/java/com/openclassrooms/P5/DataLoader.java
@@ -2,8 +2,12 @@ package com.openclassrooms.P5;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.asm.TypeReference;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -20,6 +24,14 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         JsonNode jsonDataFile;
+
+        try (InputStream inputStream = TypeReference.class.getResourceAsStream("/data/data.json")) {
+            jsonDataFile = objectMapper.readValue(inputStream, JsonNode.class);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read JSON data", e);
+        }
+
+        System.out.println(jsonDataFile);
 
 
     }
