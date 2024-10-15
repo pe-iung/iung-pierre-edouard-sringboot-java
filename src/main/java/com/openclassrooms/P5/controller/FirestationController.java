ca01759e787a609d5930e4569e3f1af28b0410c6
pierre-edouard iung <contact@pierre-edouard.tech>
2024-10-15 18:22:07 +0200

    feat: add controller to get all firestationsDTO

--- src/main/java/com/openclassrooms/P5/controller/FirestationController.java
+++ src/main/java/com/openclassrooms/P5/controller/FirestationController.java
@@ -1,4 +1,13 @@
 package com.openclassrooms.P5.controller;
+
+import com.openclassrooms.P5.dto.FirestationDTO;
+import com.openclassrooms.P5.service.FirestationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /*
 http://localhost:8080/firestation?stationNumber=<station_number>
 Cette url doit retourner une liste des personnes couvertes par la caserne de pompiers
@@ -8,6 +17,14 @@ suivantes : prénom, nom, adresse, numéro de téléphone. De plus, elle doit fo
 décompte du nombre d'adultes et du nombre d'enfants (tout individu âgé de 18 ans ou
 moins) dans la zone desservie.
  */
-
+@RestController
 public class FirestationController {
+
+    @Autowired
+    private FirestationService firestationService;
+
+    @GetMapping("/firestations")
+    public List<FirestationDTO> getAllFirestations(){
+        return firestationService.getAllFirestationDTO();
+    }
 }
--- src/main/java/com/openclassrooms/P5/repository/FirestationRepository.java
+++ src/main/java/com/openclassrooms/P5/repository/FirestationRepository.java
@@ -1,7 +1,6 @@
 package com.openclassrooms.P5.repository;
 
 import com.openclassrooms.P5.model.Firestation;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
