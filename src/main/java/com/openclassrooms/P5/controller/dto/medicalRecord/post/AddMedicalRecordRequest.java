efc175312a679846a4355fed84fc200c7dcd1e0a
pierre-edouard iung <contact@pierre-edouard.tech>
2024-10-22 13:00:21 +0200

    feat: add controller for POST medicalRecord

--- src/main/java/com/openclassrooms/P5/controller/dto/medicalRecord/post/AddMedicalRecordRequest.java
+++ src/main/java/com/openclassrooms/P5/controller/dto/medicalRecord/post/AddMedicalRecordRequest.java
@@ -1,4 +1,40 @@
 package com.openclassrooms.P5.controller.dto.medicalRecord.post;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * example paylaod
+ *       "firstName": "John",
+ *       "lastName": "Boyd",
+ *       "birthdate": "03/06/1984",
+ *       "medications": [
+ *         "aznol:350mg",
+ *         "hydrapermazol:100mg"
+ *       ],
+ *       "allergies": [
+ *         "nillacilan"
+ *       ]
+ */
+
+@Value
 public class AddMedicalRecordRequest {
+    @NotBlank(message = "firstName can not be null, empty or blank")
+    private String firstName;
+
+    @NotBlank(message = "lastName can not be null, empty or blank")
+    private String lastName;
+
+    @NotBlank(message = "birthdate can not be null, empty or blank and should follow this pattern MM/dd/yyyy")
+    @JsonFormat(pattern = "MM/dd/yyyy")
+    private LocalDate birthdate;
+
+    private List<String> medications;
+    private List<String> allergies;
+
 }
--- src/main/java/com/openclassrooms/P5/controller/dto/medicalRecord/post/MedicalRecordAddedResponse.java
+++ src/main/java/com/openclassrooms/P5/controller/dto/medicalRecord/post/MedicalRecordAddedResponse.java
@@ -1,4 +1,28 @@
 package com.openclassrooms.P5.controller.dto.medicalRecord.post;
 
+
+import com.openclassrooms.P5.model.Firestation;
+import com.openclassrooms.P5.model.MedicalRecord;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Value
 public class MedicalRecordAddedResponse {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthdate;
+    private List<String> medications;
+    private List<String> allergies;
+
+    public MedicalRecordAddedResponse(MedicalRecord medicalRecord){
+        this.firstName = medicalRecord.getFirstName();
+        this.lastName = medicalRecord.getLastName();
+        this.birthdate = medicalRecord.getBirthdate();
+        this.medications = medicalRecord.getMedications();
+        this.allergies = medicalRecord.getAllergies();
+
+    }
 }
