07a57969ea4753efc3cb1c6a0914273093833069
pierre-edouard iung <contact@pierre-edouard.tech>
2024-11-12 18:00:23 +0100

    feat: add interfaces methods for MedicalRecords and Persons

--- src/main/java/com/openclassrooms/P5/service/MedicalRecordService.java
+++ src/main/java/com/openclassrooms/P5/service/MedicalRecordService.java
@@ -1,4 +1,9 @@
 package com.openclassrooms.P5.service;
 
+import com.openclassrooms.P5.model.MedicalRecord;
+
 public interface MedicalRecordService {
+    public MedicalRecord saveMedicalRecord(MedicalRecord medicalRecord);
+    public void deleteMedicalRecordById(String id);
+
 }
--- src/main/java/com/openclassrooms/P5/service/PersonService.java
+++ src/main/java/com/openclassrooms/P5/service/PersonService.java
@@ -1,4 +1,8 @@
 package com.openclassrooms.P5.service;
 
+import com.openclassrooms.P5.model.Person;
+
 public interface PersonService {
+    public Person savePerson(Person person);
+    public void deletePersonById(String id);
 }
