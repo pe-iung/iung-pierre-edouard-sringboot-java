b0306ce55cbf00ac90e919ea4ffd6e61f5dc6df3
pierre-edouard iung <contact@pierre-edouard.tech>
2024-12-02 14:14:59 +0100

    chore: bugfix wrong http parameter types

--- src/main/java/com/openclassrooms/P5/dto/urls/childAlert/ChildAlertController.java
+++ src/main/java/com/openclassrooms/P5/dto/urls/childAlert/ChildAlertController.java
@@ -7,10 +7,7 @@ import com.openclassrooms.P5.service.PersonServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,7 +17,7 @@ public class ChildAlertController {
     private final PersonServiceImpl personServiceImpl;
 
     @GetMapping("/childAlert")
-    public ResponseEntity<?> childAlertByAddress(@PathVariable String address) {
+    public ResponseEntity<?> childAlertByAddress(@RequestParam String address) {
         List<Child> childAlertList = personServiceImpl.childAlertByAddress(address);
         return ResponseEntity.status(HttpStatus.OK).body(childAlertList);
     }
