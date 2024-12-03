32ec1aa0697b22d468e12893ac998da354f1877c
pierre-edouard iung <contact@pierre-edouard.tech>
2024-12-03 12:49:06 +0100

    feat: add new IT for FireAlert

---
+++ src/test/java/com/openclassrooms/P5/controller/FireAlertIT.java
@@ -0,0 +1,46 @@
+package com.openclassrooms.P5.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openclassrooms.P5.repository.PersonRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class FireAlertIT {
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testFireAlert() throws Exception {
+        //given an address and an expected response
+        String addressRequested = "908 73rd St";
+
+
+        //when a call is made to the fire alert endpoint
+        final ResultActions response = mockMvc
+                .perform(get("/fire?address=" + addressRequested)
+                        .contentType(MediaType.APPLICATION_JSON));
+
+        //then the response contain expected data
+        response.andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(2)));
+                //.andExpect(jsonPath("$[0]['firstName']",));
+    }
+}
