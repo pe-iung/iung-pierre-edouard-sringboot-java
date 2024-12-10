ef9bec0f37196e53350847dc8804f2da63d951b1
pierre-edouard iung <contact@pierre-edouard.tech>
2024-12-10 20:17:51 +0100

    feat: add new error handling for controller

--- src/main/java/com/openclassrooms/P5/controller/ApiExceptionInterceptor.java
+++ src/main/java/com/openclassrooms/P5/controller/ApiExceptionInterceptor.java
@@ -1,4 +1,6 @@
 package com.openclassrooms.P5.controller;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.openclassrooms.P5.exceptions.ConflictException;
 import com.openclassrooms.P5.exceptions.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSourceResolvable;
@@ -12,7 +14,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.format.DateTimeParseException;
 import java.util.Collection;
+
 import java.time.OffsetDateTime;
 import java.util.Objects;
 
@@ -37,6 +42,23 @@ public class ApiExceptionInterceptor extends ResponseEntityExceptionHandler {
         return this.handleExceptionInternal(exception, body, headers, HttpStatus.NOT_FOUND, request);
     }
 
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<?> conflictException(ConflictException exception, WebRequest request) {
+        HttpHeaders headers = new HttpHeaders();
+        ProblemDetail body = createProblemDetail(exception, HttpStatus.CONFLICT, exception.getMessage(), null, null, request);
+        log.error("API exception handler : conflict exception ={}", body);
+        return this.handleExceptionInternal(exception, body, headers, HttpStatus.CONFLICT, request);
+    }
+
+//    @ExceptionHandler(HttpMessageNotReadableException.class)
+//    public ResponseEntity<?> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+//
+//        ProblemDetail body = createProblemDetail(exception, HttpStatus.BAD_REQUEST, exception.getMessage(), null, null, request);
+//        log.error("API exception handler : HttpMessageNotReadable ={}", body);
+//        return this.handleExceptionInternal(exception, body, headers, HttpStatus.BAD_REQUEST, request);
+//    }
+
+
 //    @ExceptionHandler(GenericApiRequestException.class)
 //    public ResponseEntity<Object> handleGenericException(GenericApiRequestException exception, WebRequest request) {
 //        HttpStatus status = switch (exception) {
@@ -54,53 +76,60 @@ public class ApiExceptionInterceptor extends ResponseEntityExceptionHandler {
 //    }
 
 
-//    @Override
-//    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-//        String[] args = exception.getBindingResult()
-//                .getAllErrors()
-//                .stream()
-//                .map(msg -> "Field '" + ((DefaultMessageSourceResolvable) Objects.requireNonNull(msg.getArguments())[0]).getDefaultMessage() + "' : " + msg.getDefaultMessage())
-//                .toArray(String[]::new);
-//
-//
-//        String defaultDetail = "Argument not valid";
-//        ProblemDetail body = this.createProblemDetail(exception, status, defaultDetail, null, args, request);
-//        return this.handleExceptionInternal(exception, body, headers, status, request);
-//    }
-//
-//    @Nullable
-//    protected ResponseEntity<Object> handleHandlerMethodValidationException(HandlerMethodValidationException exception, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-//        var errorList = exception.getAllValidationResults()
-//                .stream()
-//                .map(ParameterValidationResult::getResolvableErrors)
-//                .flatMap(Collection::stream)
-//                .map(MessageSourceResolvable::getDefaultMessage)
-//                .toArray(String[]::new);
-//
-//        String defaultDetail = "Argument not valid";
-//
-//        ProblemDetail body = this.createProblemDetail(exception, status, defaultDetail, null, errorList, request);
-//        return this.handleExceptionInternal(exception, body, headers, status, request);
-//    }
-//
-//    @Override
-//    protected ProblemDetail createProblemDetail(Exception exception, HttpStatusCode status, String defaultDetail, @Nullable String detailMessageCode, @Nullable Object[] detailMessageArguments, WebRequest request) {
-//        if(status.is5xxServerError()) {
-//            log.error("{} - {}", exception.getClass().getSimpleName(), exception.getMessage());
-//        } else {
-//            log.warn("{} - {}", exception.getClass().getSimpleName(), exception.getMessage());
-//        }
-//
-//        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, defaultDetail);
-//        problemDetail.setProperty("timestamp", OffsetDateTime.now());
-//        if (detailMessageCode != null) {
-//            problemDetail.setProperty("messageCode", detailMessageCode);
-//        }
-//        if (detailMessageArguments != null) {
-//            problemDetail.setProperty("details", detailMessageArguments);
-//        }
-//
-//        return problemDetail;
-//    }
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        String[] args = exception.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(msg -> "Field '" + ((DefaultMessageSourceResolvable) Objects.requireNonNull(msg.getArguments())[0]).getDefaultMessage() + "' : " + msg.getDefaultMessage())
+                .toArray(String[]::new);
+
+
+        String defaultDetail = "Argument not valid";
+        ProblemDetail body = this.createProblemDetail(exception, status, defaultDetail, null, args, request);
+        return this.handleExceptionInternal(exception, body, headers, status, request);
+    }
+
+    @Nullable
+    protected ResponseEntity<Object> handleHandlerMethodValidationException(HandlerMethodValidationException exception, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        var errorList = exception.getAllValidationResults()
+                .stream()
+                .map(ParameterValidationResult::getResolvableErrors)
+                .flatMap(Collection::stream)
+                .map(MessageSourceResolvable::getDefaultMessage)
+                .toArray(String[]::new);
+
+        String defaultDetail = "Argument not valid";
+
+        ProblemDetail body = this.createProblemDetail(exception, status, defaultDetail, null, errorList, request);
+        return this.handleExceptionInternal(exception, body, headers, status, request);
+    }
+
+
+
+
+
+
+
+
+    @Override
+    protected ProblemDetail createProblemDetail(Exception exception, HttpStatusCode status, String defaultDetail, @Nullable String detailMessageCode, @Nullable Object[] detailMessageArguments, WebRequest request) {
+        if(status.is5xxServerError()) {
+            log.error("{} - {}", exception.getClass().getSimpleName(), exception.getMessage());
+        } else {
+            log.warn("{} - {}", exception.getClass().getSimpleName(), exception.getMessage());
+        }
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, defaultDetail);
+        problemDetail.setProperty("timestamp", OffsetDateTime.now());
+        if (detailMessageCode != null) {
+            problemDetail.setProperty("messageCode", detailMessageCode);
+        }
+        if (detailMessageArguments != null) {
+            problemDetail.setProperty("details", detailMessageArguments);
+        }
+
+        return problemDetail;
+    }
 
 }
---
+++ src/main/java/com/openclassrooms/P5/exceptions/ConflictException.java
@@ -0,0 +1,12 @@
+package com.openclassrooms.P5.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ConflictException extends RuntimeException{
+    public ConflictException(String message) {
+        super(message);
+        log.error("Conflict exception = {}",message);
+    }
+
+}
